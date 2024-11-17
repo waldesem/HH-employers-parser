136f7dc83135baa4632af5f76805157deb9d501b
Vladimir Semenenko <wsemenenko@gmail.com>
2024-11-17 15:09:53 +0300

    Update code

--- hh.py
+++ hh.py
@@ -2,8 +2,9 @@ import asyncio
 import csv
 import sqlite3
 from enum import Enum
+from itertools import chain
 
-import aiohttp
+from aiohttp import ClientSession
 
 
 class Employers(Enum):
@@ -14,38 +15,47 @@ class Employers(Enum):
 
 
 url = "https://api.hh.ru/vacancies"
-
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.101.76 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
 }
+pages = 20  # not more
+period = 1
 
-pages = 20 # not more
 
-
-async def fetch_vacancies(session, employer, page):
+async def fetch_vacancies(
+    session: ClientSession, employer: Employers, page: int
+) -> list[tuple[str, str, str]]:
     params = {
         "id": employer.value,
         "area": 1,
         "employment": "full",
-        "schedule": ["fullDay", "flexible", "remote"],
+        "schedule": ["fullDay", "remote"],
         "experience": ["between1And3", "between3And6", "moreThan6"],
-        "period": 1,
+        "period": period,
         "page": page,
         "per_page": 100,
-        "vacancy_search_order": "publication_time"
+        "vacancy_search_order": "publication_time",
     }
     async with session.get(url, params=params, headers=headers) as response:
         data = await response.json()
-        return [(employer.name, item["name"], item["published_at"]) for item in data["items"]]
+        return [
+            (employer.name, item.get("name"), item.get("published_at"))
+            for item in data.get("items", [])
+        ]
 
 
 async def main():
-    async with aiohttp.ClientSession() as session:
+    async with ClientSession() as session:
         results = []
         for employer in Employers:
-            for i in range(1, pages):
-                items = await fetch_vacancies(session, employer, i)
-                results.extend(items)
+            items = await asyncio.gather(
+                *[
+                    fetch_vacancies(session, employer, page)
+                    for page in range(pages + 1)
+                ]
+            )
+            results.extend(chain.from_iterable(items))
+            print(f"done {employer}")
 
         # write to csv
         with open("hh.csv", "a", newline="", encoding="utf-8") as f:
@@ -56,7 +66,6 @@ async def main():
         # write to db
         with sqlite3.connect("hh.db") as con:
             cur = con.cursor()
-
             cur.execute("""CREATE TABLE IF NOT EXISTS vacancies(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 employer TEXT,
@@ -67,8 +76,11 @@ async def main():
                 "INSERT INTO vacancies (employer, vacancy, published_at) VALUES (?, ?, ?)",
                 results,
             )
-
             con.commit()
+
+        print("done all")
 
 
 asyncio.run(main())
+
+lst = [["employer", "vacancy", "published_at"], ["employer", "vacancy", "published_at"]]
